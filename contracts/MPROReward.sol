11385e6d1cc4dded3e34958dd6c6fba4cffc7528
Jakub Antczak <jakubantczak.info@gmail.com>
2024-04-18 12:12:08 +0200

    MPRO-001: Rewards

--- contracts/MPROReward.sol
+++ contracts/MPROReward.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MPROReward is Ownable, AccessControl {
     bytes32 public constant MPRO_MASTER_DISTRIBUTOR_ROLE =
         keccak256("MPRO_MASTER_DISTRIBUTOR_ROLE");
 
     struct Reward {
-        address _rewardToken;
         uint256 _rewardToClaim;
         uint256 _claimedReward;
         uint256 _lastRewardTimestamp;
@@ -25,30 +25,66 @@ contract MPROReward is Ownable, AccessControl {
 
     mapping(address => Claim[]) private userClaimHistory;
 
-    constructor(address _initialOwner) {
+    uint256 private tokenBalanceForReward;
+
+    address claimTokenAddress;
+
+    ERC20 claimToken;
+
+    constructor(address _initialOwner, address _claimTokenAddress) {
+        claimTokenAddress = _claimTokenAddress;
         _transferOwnership(_initialOwner);
     }
 
     function addReward(
-        address _rewardToken,
-        uint256 _rewardAmount,
+        uint256 _rewardAmountInRewardToken,
+        uint256 _addRewardTxCostInRewardToken,
         address _claimer
     ) public onlyOwner {
         Reward memory reward = userRewards[_claimer];
+        require(
+            reward._lastRewardTimestamp + 1 days < block.timestamp,
+            "You can claim reward once a day"
+        );
+
+        uint256 rewardTokenBalance = ERC20(claimTokenAddress).balanceOf(
+            address(this)
+        );
+
+        require(
+            rewardTokenBalance >=
+                _rewardAmountInRewardToken + _rewardAmountInRewardToken,
+            "Not enough reward token balance"
+        );
+
         uint256 currentRewardToClaim = reward._rewardToClaim;
-        currentRewardToClaim += _rewardAmount;
-        userRewards[_rewardToken] = Reward({
-            _rewardToken: _rewardToken,
+
+        currentRewardToClaim +=
+            _rewardAmountInRewardToken -
+            _addRewardTxCostInRewardToken;
+        userRewards[msg.sender] = Reward({
             _rewardToClaim: currentRewardToClaim,
             _claimedReward: reward._claimedReward,
             _lastRewardTimestamp: reward._lastRewardTimestamp
         });
+
+        tokenBalanceForReward += _rewardAmountInRewardToken;
     }
 
     function claimReward(address _claimer) public {
-        Reward memory reward = userRewards[_claimer];
+        Reward storage reward = userRewards[_claimer];
         require(reward._rewardToClaim > 0, "No reward to claim");
         uint256 rewardToClaim = reward._rewardToClaim;
+
+        ERC20(claimTokenAddress).transferFrom(
+            _claimer,
+            address(this),
+            rewardToClaim
+        );
+
+        reward._claimedReward += rewardToClaim;
+        reward._rewardToClaim = 0;
+        reward._lastRewardTimestamp = block.timestamp;
 
         userClaimHistory[_claimer].push(
             Claim({
@@ -57,5 +93,13 @@ contract MPROReward is Ownable, AccessControl {
                 _claimTimestamp: block.timestamp
             })
         );
+
+        tokenBalanceForReward -= rewardToClaim;
+    }
+
+    function getClaimHistory(
+        address _claimer
+    ) public view returns (Claim[] memory) {
+        return userClaimHistory[_claimer];
     }
 }
