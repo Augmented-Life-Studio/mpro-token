32eab7dfcbf7dd70707d3252b3bf8df3d07ce43b
Jakub Antczak <jakubantczak.info@gmail.com>
2024-05-21 10:37:52 +0200

    MPRO-001: Added testVesting script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,43 +1,40 @@
-require("dotenv").config()
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-ethers";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy"
-import "@nomiclabs/hardhat-web3"
-import "hardhat-deploy-ethers"
-import "./tasks/index"
-import 'solidity-coverage';
-
+require('dotenv').config()
+import {HardhatUserConfig} from 'hardhat/config'
+import '@nomicfoundation/hardhat-ethers'
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-deploy'
+import '@nomiclabs/hardhat-web3'
+import 'hardhat-deploy-ethers'
+import './tasks/index'
+import 'solidity-coverage'
 
 const {
 	bscscanApiKey,
 	polygonMumbaiApiKey,
 	ethApiKey,
 	arbiApiKey,
-	baseApiKey
+	baseApiKey,
 } = require('./secrets.json')
-
 
 function getMnemonic(networkName: string) {
 	if (networkName) {
-		const mnemonic = process.env["MNEMONIC_" + networkName.toUpperCase()]
-		if (mnemonic && mnemonic !== "") {
+		const mnemonic = process.env['MNEMONIC_' + networkName.toUpperCase()]
+		if (mnemonic && mnemonic !== '') {
 			return mnemonic
 		}
 	}
 
 	const mnemonic = process.env.MNEMONIC
-	if (!mnemonic || mnemonic === "") {
-		return "test test test test test test test test test test test junk"
+	if (!mnemonic || mnemonic === '') {
+		return 'test test test test test test test test test test test junk'
 	}
 
 	return mnemonic
 }
 
 function accounts(chainKey: string) {
-	return { mnemonic: getMnemonic(chainKey) }
+	return {mnemonic: getMnemonic(chainKey)}
 }
-
 
 const config: HardhatUserConfig = {
 	namedAccounts: {
@@ -47,12 +44,12 @@ const config: HardhatUserConfig = {
 		},
 		// Owner of the contract
 		owner: {
-			default: "0x03A1b656565E7c20aA4fadD4338f5Fa73585a62b", // wallet address 0, of the mnemonic in .env
+			default: '0x03A1b656565E7c20aA4fadD4338f5Fa73585a62b', // wallet address 0, of the mnemonic in .env
 		},
 		// Treasury address
 		treasury: {
-			default: "0x68E5CF81eb3c319e47006EAe067E04ebf6610204"
-		}
+			default: '0x68E5CF81eb3c319e47006EAe067E04ebf6610204',
+		},
 	},
 	gasReporter: {
 		currency: 'USD',
@@ -87,80 +84,90 @@ const config: HardhatUserConfig = {
 	},
 	networks: {
 		ethereum: {
-			url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161", // public infura endpoint
+			url: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161', // public infura endpoint
 			chainId: 1,
-			accounts: accounts("ethereum"),
+			accounts: accounts('ethereum'),
 		},
 		base: {
-			url: "https://base.llamarpc.com",
+			url: 'https://base.llamarpc.com',
 			chainId: 8453,
-			accounts: accounts("base"),
+			accounts: accounts('base'),
 		},
 		bsc: {
-			url: "https://bsc-dataseed1.binance.org",
+			url: 'https://bsc-dataseed1.binance.org',
 			chainId: 56,
-			accounts: accounts("bsc"),
+			accounts: accounts('bsc'),
 		},
 		avalanche: {
-			url: "https://api.avax.network/ext/bc/C/rpc",
+			url: 'https://api.avax.network/ext/bc/C/rpc',
 			chainId: 43114,
-			accounts: accounts("avalanche"),
+			accounts: accounts('avalanche'),
 		},
 		polygon: {
-			url: "https://rpc-mainnet.matic.quiknode.pro",
+			url: 'https://rpc-mainnet.matic.quiknode.pro',
 			chainId: 137,
-			accounts: accounts("polygon"),
+			accounts: accounts('polygon'),
 		},
 		arbitrum: {
 			url: `https://arb1.arbitrum.io/rpc`,
 			chainId: 42161,
-			accounts: accounts("arbitrum"),
+			accounts: accounts('arbitrum'),
 		},
 		optimism: {
 			url: `https://mainnet.optimism.io`,
 			chainId: 10,
-			accounts: accounts("optimism"),
+			accounts: accounts('optimism'),
 		},
 		fantom: {
 			url: `https://rpcapi.fantom.network`,
 			chainId: 250,
-			accounts: accounts("fantom"),
+			accounts: accounts('fantom'),
 		},
 		metis: {
 			url: `https://andromeda.metis.io/?owner=1088`,
 			chainId: 1088,
-			accounts: accounts("metis"),
+			accounts: accounts('metis'),
+		},
+		sepolia: {
+			url: `https://ethereum-sepolia-rpc.publicnode.com`,
+			chainId: 11155111,
+			accounts: accounts('sepolia'),
 		},
 
-		"bsc-testnet": {
-			url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+		'bsc-testnet': {
+			url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
 			chainId: 97,
-			accounts: accounts("bsc-testnet"),
+			accounts: accounts('bsc-testnet'),
 		},
 		fuji: {
 			url: `https://api.avax-test.network/ext/bc/C/rpc`,
 			chainId: 43113,
-			accounts: accounts("fuji"),
+			accounts: accounts('fuji'),
 		},
 		mumbai: {
-			url: "https://polygon-testnet.public.blastapi.io",
+			url: 'https://polygon-testnet.public.blastapi.io',
 			chainId: 80001,
-			accounts: accounts("mumbai"),
+			accounts: accounts('mumbai'),
 		},
-		"arbitrum-sepolia": {
+		'arbitrum-sepolia': {
 			url: `https://goerli-rollup.arbitrum.io/rpc/`,
 			chainId: 421613,
-			accounts: accounts("arbitrum-goerli"),
+			accounts: accounts('arbitrum-goerli'),
 		},
-		"optimism-sepolia": {
+		'optimism-sepolia': {
 			url: `https://goerli.optimism.io/`,
 			chainId: 420,
-			accounts: accounts("optimism-goerli"),
+			accounts: accounts('optimism-goerli'),
 		},
-		"fantom-testnet": {
+		'fantom-testnet': {
 			url: `https://rpc.ankr.com/fantom_testnet`,
 			chainId: 4002,
-			accounts: accounts("fantom-testnet"),
+			accounts: accounts('fantom-testnet'),
+		},
+		'base-sepolia': {
+			url: `https://sepolia.base.org`,
+			chainId: 84532,
+			accounts: accounts('base-sepolia'),
 		},
 	},
 	mocha: {
@@ -174,10 +181,13 @@ const config: HardhatUserConfig = {
 			polygon: polygonMumbaiApiKey,
 			polygonMumbai: polygonMumbaiApiKey,
 			mainnet: ethApiKey,
-			arbitrumOne: arbiApiKey,
+			sepolia: ethApiKey,
+			arbitrum: arbiApiKey,
+			arbitrumSepolia: arbiApiKey,
 			base: baseApiKey,
+			baseSepolia: baseApiKey,
 		},
 	},
-};
+}
 
-export default config;
+export default config
