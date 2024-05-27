214176521ed227d0345afb475c5a64b44b528486
Jakub Antczak <jakubantczak.info@gmail.com>
2024-05-27 11:04:44 +0200

    MPRO-001: Updated priv and adv vesting tables to update

--- deploy/adv.ts
+++ deploy/adv.ts
@@ -1,53 +1,63 @@
+import {DeploymentsExtension} from 'hardhat-deploy/dist/types'
+import addresses from '../vestingAddresses/advisorsBenaficiariesExtra27052024.json'
+import {ethers} from 'hardhat'
 
-import { DeploymentsExtension } from "hardhat-deploy/dist/types";
-import addresses from "../vestingAddresses/advisorsBenaficiaries.json"
-import { ethers } from "hardhat";
+const VESTING_CONTRACT_NAME = 'AdvisiorsVesting'
 
-const VESTING_CONTRACT_NAME = "AdvisiorsVesting"
-
-const MPRO_ADDRESS = "0xd88611a629265c9af294ffdd2e7fa4546612273e"
+const MPRO_ADDRESS = '0xd88611a629265c9af294ffdd2e7fa4546612273e'
 const TGE_UNLOCK_TIMESTAMP = 1713362400 // Wed Apr 17 2024 12:00:00 GMT+0000
 const TGE_UNLOCK_PERCENT = 500 // 5%
 const CLIFF_DELAY = 7889229 // 3 months
 const VESTING_UNLOCK_PERCENT_PER_PERIOD = 396 // 7.91%
 const VESTING_PERIOD_DURATION = 2629743 // 1 month
 
-module.exports = async function ({ deployments, getNamedAccounts }: {
-    deployments: DeploymentsExtension, getNamedAccounts: any
+module.exports = async function ({
+	deployments,
+	getNamedAccounts,
+}: {
+	deployments: DeploymentsExtension
+	getNamedAccounts: any
 }) {
-    const { deployer } = await getNamedAccounts()
+	const {deployer} = await getNamedAccounts()
 
-    const { deploy } = deployments
+	const {deploy} = deployments
 
-    const vesting = await deploy(VESTING_CONTRACT_NAME, {
-        from: deployer,
-        args: [
-            TGE_UNLOCK_TIMESTAMP,
-            TGE_UNLOCK_PERCENT,
-            CLIFF_DELAY,
-            VESTING_UNLOCK_PERCENT_PER_PERIOD,
-            VESTING_PERIOD_DURATION,
-            deployer
-        ],
-        log: true,
-        waitConfirmations: 1,
-        skipIfAlreadyDeployed: true
-    })
+	const vesting = await deploy(VESTING_CONTRACT_NAME, {
+		from: deployer,
+		args: [
+			TGE_UNLOCK_TIMESTAMP,
+			TGE_UNLOCK_PERCENT,
+			CLIFF_DELAY,
+			VESTING_UNLOCK_PERCENT_PER_PERIOD,
+			VESTING_PERIOD_DURATION,
+			deployer,
+		],
+		log: true,
+		waitConfirmations: 1,
+		skipIfAlreadyDeployed: true,
+	})
 
-    console.log(`AdvisiorsVesting ${VESTING_CONTRACT_NAME} deployed to:`, vesting.address);
+	console.log(
+		`AdvisiorsVesting ${VESTING_CONTRACT_NAME} deployed to:`,
+		vesting.address,
+	)
 
-    const addrs = addresses.map((el: any) => el.Adress)
-    const amounts = addresses.map((el: any) => ethers.parseEther(el.Amount))
+	const ves = await ethers.getContractAt(
+		`${VESTING_CONTRACT_NAME}`,
+		vesting.address,
+	)
 
-    const ves = await ethers.getContractAt(`${VESTING_CONTRACT_NAME}`, vesting.address);
+	const vestingToken = await ves.token()
 
-    await ves.setVestingToken(MPRO_ADDRESS, { from: deployer })
+	if (vestingToken.toLowerCase() !== MPRO_ADDRESS.toLowerCase()) {
+		await ves.setVestingToken(MPRO_ADDRESS, {from: deployer})
+		console.log(`Vesting token set to ${MPRO_ADDRESS}`)
+	}
 
-    await ves.registerBeneficiaries(
-        addrs,
-        amounts,
-        { from: deployer }
-    )
+	const addrs = addresses.map((el: any) => el.Adress)
+	const amounts = addresses.map((el: any) => ethers.parseEther(el.Amount))
+
+	await ves.registerBeneficiaries(addrs, amounts, {from: deployer})
 }
 
-module.exports.tags = ["AdvisiorsVesting"]
+module.exports.tags = ['AdvisiorsVesting']
--- deploy/priv.ts
+++ deploy/priv.ts
@@ -1,52 +1,62 @@
-import { DeploymentsExtension } from "hardhat-deploy/dist/types";
-import addresses from "../vestingAddresses/privateBenaficiaries.json"
-import { ethers } from "hardhat";
+import {DeploymentsExtension} from 'hardhat-deploy/dist/types'
+import addresses from '../vestingAddresses/privateBenaficiariesExtra27052024.json'
+import {ethers} from 'hardhat'
 
-const VESTING_CONTRACT_NAME = "PrivateRoundVesting"
+const VESTING_CONTRACT_NAME = 'PrivateRoundVesting'
 
-const MPRO_ADDRESS = "0xd88611a629265c9af294ffdd2e7fa4546612273e"
+const MPRO_ADDRESS = '0xd88611a629265c9af294ffdd2e7fa4546612273e'
 const TGE_UNLOCK_TIMESTAMP = 1713362400 // Wed Apr 17 2024 12:00:00 GMT+0000
 const TGE_UNLOCK_PERCENT = 700 // 5%
 const CLIFF_DELAY = 5259486 // 2 months
 const VESTING_UNLOCK_PERCENT_PER_PERIOD = 930 // 7.91%
 const VESTING_PERIOD_DURATION = 2629743 // 1 month
 
-module.exports = async function ({ deployments, getNamedAccounts }: {
-    deployments: DeploymentsExtension, getNamedAccounts: any
+module.exports = async function ({
+	deployments,
+	getNamedAccounts,
+}: {
+	deployments: DeploymentsExtension
+	getNamedAccounts: any
 }) {
-    const { deployer } = await getNamedAccounts()
+	const {deployer} = await getNamedAccounts()
 
-    const { deploy } = deployments
+	const {deploy} = deployments
 
-    const vesting = await deploy(VESTING_CONTRACT_NAME, {
-        from: deployer,
-        args: [
-            TGE_UNLOCK_TIMESTAMP,
-            TGE_UNLOCK_PERCENT,
-            CLIFF_DELAY,
-            VESTING_UNLOCK_PERCENT_PER_PERIOD,
-            VESTING_PERIOD_DURATION,
-            deployer
-        ],
-        log: true,
-        waitConfirmations: 1,
-        skipIfAlreadyDeployed: true
-    })
+	const vesting = await deploy(VESTING_CONTRACT_NAME, {
+		from: deployer,
+		args: [
+			TGE_UNLOCK_TIMESTAMP,
+			TGE_UNLOCK_PERCENT,
+			CLIFF_DELAY,
+			VESTING_UNLOCK_PERCENT_PER_PERIOD,
+			VESTING_PERIOD_DURATION,
+			deployer,
+		],
+		log: true,
+		waitConfirmations: 1,
+		skipIfAlreadyDeployed: true,
+	})
 
-    console.log(`PrivateRoundVesting ${VESTING_CONTRACT_NAME} deployed to:`, vesting.address);
+	console.log(`${VESTING_CONTRACT_NAME} deployed to:`, vesting.address)
 
-    const addrs = addresses.map((el: any) => el.Adress)
-    const amounts = addresses.map((el: any) => ethers.parseEther(String(el.Amount)))
+	const ves = await ethers.getContractAt(
+		`${VESTING_CONTRACT_NAME}`,
+		vesting.address,
+	)
 
-    const ves = await ethers.getContractAt(`${VESTING_CONTRACT_NAME}`, vesting.address);
+	const vestingToken = await ves.token()
 
-    await ves.setVestingToken(MPRO_ADDRESS, { from: deployer })
+	if (vestingToken.toLowerCase() !== MPRO_ADDRESS.toLowerCase()) {
+		await ves.setVestingToken(MPRO_ADDRESS, {from: deployer})
+		console.log(`Vesting token set to ${MPRO_ADDRESS}`)
+	}
 
-    await ves.registerBeneficiaries(
-        addrs,
-        amounts,
-        { from: deployer }
-    )
+	const addrs = addresses.map((el: any) => el.Adress)
+	const amounts = addresses.map((el: any) =>
+		ethers.parseEther(String(el.Amount)),
+	)
+
+	await ves.registerBeneficiaries(addrs, amounts, {from: deployer})
 }
 
-module.exports.tags = ["PrivateRoundVesting"]
+module.exports.tags = ['PrivateRoundVesting']
